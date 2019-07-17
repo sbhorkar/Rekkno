1cd17bbae2bf3c11087c353e00162448786e83ed
Rekkno <rekkno.inc@gmail.com>
2019-07-17 11:31:03 -0700

    Rekkno Added AppIcon and Background 7/17/2019

---
+++ RekknoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RekknoApp
+//
+//  Created by Sanaa Bhorkar on 7/16/19.
+//  Copyright © 2019 Rekkno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
