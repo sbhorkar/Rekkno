09f86e3e0625de7829e92b08608538ca41493fca
Rekkno <rekkno.inc@gmail.com>
2019-07-17 22:05:40 -0700

    Lunch Screen, Tried with Figma 7/17/19

---
+++ RekknoApp/ViewController2.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController2.swift
+//  RekknoApp
+//
+//  Created by Sanaa Bhorkar on 7/16/19.
+//  Copyright © 2019 Rekkno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+}
+
